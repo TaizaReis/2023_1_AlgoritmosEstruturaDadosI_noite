e5ee50ae4dffdc97655494d6cc49bddf9669e019
Taiza Luana Reis do Carmo <taizareis16@gmail.com>
2023-04-24 21:28:13 -0300

    Aula sobre ClassesAbstratas

---
+++ Aula08-ClassesAbstratas/Exercicio/ContaBancaria.py
@@ -0,0 +1,10 @@
+#implementar a classe abstrata ContaBancaria com os
+# métodos cadastrar e depositar e as respectivas classes concretas.
+class ContaBancaria():
+    def __init__(self, ContaCorrente = None, ContaPoupança = None):
+        self.ContaCorrente = ContaCorrente
+        self.ContaPoupança = ContaPoupança
+        
+    
+
+
